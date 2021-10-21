2279f6d48304a470fdb733beb1ba874c273033d6
William Wells <whanwells@gmail.com>
2021-10-21 18:27:17 -0500

    Configure module path aliases

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
     "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^~/pages/(.*)$": "<rootDir>/pages/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
