da375149d6ef8ef431b4f8a3d93dfd8236f0d688
William Wells <whanwells@gmail.com>
2021-10-21 18:43:55 -0500

    Update development environment (#9)
    * Extract Babel config
    * Add missing mocks
    * Add sample test
    * Configure module path aliases
    * Bump dependencies

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,12 @@ module.exports = {
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
     "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
     "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^~/pages/(.*)$": "<rootDir>/pages/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   transformIgnorePatterns: [
     "/node_modules/",
