85cbedac29eb47f4b6cdefd55401f46005fb05bc
William Wells <whanwells@gmail.com>
2021-10-21 20:02:36 -0500

    Fix HMR issue with database

--- src/pages/api/ping.js
+++ src/pages/api/ping.js
@@ -1,8 +1,8 @@
-import { createConnection } from "typeorm";
+import { prepareConnection } from "~/database";
 
 export default async function handler(req, res) {
   try {
-    await createConnection();
+    await prepareConnection();
     res.status(200).json({ status: "ok" });
   } catch (error) {
     res.status(500).json({ error });
