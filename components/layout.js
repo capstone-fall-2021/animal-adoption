16adf9a73072191915cec555bd509e13e2a451d8
stillaschoolgirl <stillaschoolgirl@yahoo.com>
2021-10-21 22:24:47 -0500

    Create layout for homepage

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
+import styles from './layout.module.css'
 
 const websiteName = "Animal Adoption Dating App"
 
 export default function Layout({children, home}) {
     return <div>
         <header>
-            {home ? (
-                <h1>{websiteName}</h1>
-            ): (
-                <h1>not home</h1>
-            )}
+            <div className={styles.top}>
+                <div className={styles.logo}>
+                    Logo
+                </div>
+                <div>
+                    <h1>{websiteName}</h1>
+                </div>
+                <div className={styles.account}>
+                    Account | Login | Logout
+                </div>
+            </div>
         </header>
+        <main>{children}</main>
 
     </div>
 }
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import Layout from "components/layout";
 export default function Home(){
     return (
         <Layout home>
-            <h2>Hello</h2>
+            <div>Animals</div>
+            <div>News</div>
         </Layout>
     )
 }
