31dd12c024df7aea001577abbe53ed8747011dde
stillaschoolgirl <stillaschoolgirl@yahoo.com>
2021-10-28 20:03:33 -0500

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,33 @@ export default function Home() {
   return (
     <Layout home>
       <div className={styles.section}>
-        <div>Animals</div>
-        <hr />
-        <div>News</div>
+        <div className={styles.section}>
+          <div className={styles.news_item}>
+            <div className={styles.align_center}>
+              <div>animal name 1</div>
+              <div className={styles.section}>Image</div>
+            </div>
+          </div>
+          <div className={styles.news_item}>
+            <div className={styles.align_center}>
+              <div>animal name 2</div>
+              <div className={styles.section}>Image</div>
+            </div>
+          </div>
+          <div className={styles.news_item}>
+            <div className={styles.align_center}>
+              <div>animal name 3</div>
+              <div className={styles.section}>Image</div>
+            </div>
+          </div>
+        </div>
+        <hr className={styles.section}/>
+        <hr className={styles.section}/>
+        <div className={styles.section}>
+          <div className={styles.news_item}>News Item One News Item One News Item One News Item One News Item One</div>
+          <div className={styles.news_item}>News Item Two News Item Two News Item Two News Item Two News Item Two</div>
+          <div className={styles.news_item}>News Item Three News Item Three News Item Three News Item Three News Item Three</div>
+        </div>
       </div>
     </Layout>
   );
