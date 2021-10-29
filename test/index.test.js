ad4b59725d4780895fedd7d194675c01de4ddcce
asuezaki <andrewsuezaki@gmail.com>
2021-10-28 18:06:03 -0700

    changed test to test.todo()

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Home from "~/pages/index";
 
 describe("Home", () => {
-  it("renders a heading", () => {
+  it.todo("renders a heading", () => {
     render(<Home />);
 
     const heading = screen.getByRole("heading", {
