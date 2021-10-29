e119ce81b57bb466ccba5d290db68fa876837999
William Wells <whanwells@gmail.com>
2021-10-28 21:39:17 -0500

    Add NextAuth.js POC (#21)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
 import Navbar from "~/components/Navbar/Navbar";
 import Sidebar from "~/components/Navbar/Sidebar";
 import "~/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <>
+    <SessionProvider session={session}>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
