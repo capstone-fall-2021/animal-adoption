1b43fe74bfdd370af11a7b91576164925ccd9764
asuezaki <andrewsuezaki@gmail.com>
2021-11-01 20:40:13 -0700

    changes

---
+++ src/pages/profiles.js
@@ -0,0 +1,12 @@
+import Layout from "~/components/layout";
+
+export default function profiles() {
+  return (
+    <>
+      <Layout Home></Layout>
+      <div>
+        <h1>Profiles</h1>
+      </div>
+    </>
+  );
+}
