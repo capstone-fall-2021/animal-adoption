34324a1d05e8f59466bc78ea9db0ec88857a032a
William Wells <whanwells@gmail.com>
2021-11-02 20:47:06 -0500

    Refactor layout

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,15 +1,11 @@
-import Layout from "~/components/layout";
-import styles from "~/components/layout.module.css";
+import styles from "~/components/Layout.module.css";
 
 export default function Account() {
   return (
-    <>
-      <Layout Home></Layout>
-      <div className={styles.section}>
-        <div>
-          <h1>Account</h1>
-        </div>
+    <div className={styles.section}>
+      <div>
+        <h1>Account</h1>
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,42 @@
-import Layout from "~/components/layout";
-import styles from "~/components/layout.module.css";
+import styles from "~/components/Layout.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Layout Home></Layout>
+    <div className={styles.section}>
       <div className={styles.section}>
-        <div className={styles.section}>
-          <div className={styles.news_item}>
-            <div className={styles.align_center}>
-              <div>animal name 1</div>
-              <div className={styles.section}>Image</div>
-            </div>
-          </div>
-          <div className={styles.news_item}>
-            <div className={styles.align_center}>
-              <div>animal name 2</div>
-              <div className={styles.section}>Image</div>
-            </div>
-          </div>
-          <div className={styles.news_item}>
-            <div className={styles.align_center}>
-              <div>animal name 3</div>
-              <div className={styles.section}>Image</div>
-            </div>
+        <div className={styles.news_item}>
+          <div className={styles.align_center}>
+            <div>animal name 1</div>
+            <div className={styles.section}>Image</div>
           </div>
         </div>
-        <hr className={styles.section} />
-        <hr className={styles.section} />
-        <div className={styles.section}>
-          <div className={styles.news_item}>
-            News Item One News Item One News Item One News Item One News Item
-            One
+        <div className={styles.news_item}>
+          <div className={styles.align_center}>
+            <div>animal name 2</div>
+            <div className={styles.section}>Image</div>
           </div>
-          <div className={styles.news_item}>
-            News Item Two News Item Two News Item Two News Item Two News Item
-            Two
-          </div>
-          <div className={styles.news_item}>
-            News Item Three News Item Three News Item Three News Item Three News
-            Item Three
+        </div>
+        <div className={styles.news_item}>
+          <div className={styles.align_center}>
+            <div>animal name 3</div>
+            <div className={styles.section}>Image</div>
           </div>
         </div>
       </div>
-    </>
+      <hr className={styles.section} />
+      <hr className={styles.section} />
+      <div className={styles.section}>
+        <div className={styles.news_item}>
+          News Item One News Item One News Item One News Item One News Item One
+        </div>
+        <div className={styles.news_item}>
+          News Item Two News Item Two News Item Two News Item Two News Item Two
+        </div>
+        <div className={styles.news_item}>
+          News Item Three News Item Three News Item Three News Item Three News
+          Item Three
+        </div>
+      </div>
+    </div>
   );
 }
