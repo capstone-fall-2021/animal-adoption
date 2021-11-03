d9af2a126396ea9d38acc7031ee01cd147975649
stillaschoolgirl <stillaschoolgirl@yahoo.com>
2021-11-03 07:22:28 -0500

    Add database queries to the profile, home, and news pages (#29)
    * Add pages and queries
    
    * Add queries to home, news and profile pages
    
    * lint fixes

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,9 +1,32 @@
-import styles from "~/components/Layout.module.css";
+import styles from "~/components/layout.module.css";
+import { PrismaClient } from "@prisma/client";
 
-export default function News() {
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const newsAll = await prisma.news.findMany({
+    select: {
+      news: true,
+    },
+  });
+
+  newsAll.forEach((item, index) => {
+    console.log(item);
+    console.log(index);
+  });
+
+  return {
+    props: {
+      newsall: newsAll,
+    },
+  };
+};
+
+export default function News(getServerSideProps) {
   return (
     <div className={styles.section}>
       <div>News</div>
+      <div>{getServerSideProps.newsall.toString()}</div>
     </div>
   );
 }
--- src/pages/profiles.js
+++ src/pages/profiles.js
@@ -1,7 +1,94 @@
-export default function Profiles() {
+import styles from "~/components/layout.module.css";
+import { PrismaClient } from "@prisma/client";
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const profileAll = await prisma.profile.findMany({
+    select: {
+      description: true,
+      pic: true,
+      breed: {
+        select: {
+          name: true,
+          type: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+      disposition: {
+        select: {
+          description: true,
+        },
+      },
+      availability: {
+        select: {
+          description: true,
+        },
+      },
+    },
+  });
+
+  const profileDogs = await prisma.profile.findMany({
+    where: {
+      breed: {
+        type: {
+          name: "dog",
+        },
+      },
+    },
+    select: {
+      description: true,
+      pic: true,
+      breed: {
+        select: {
+          name: true,
+          type: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+      disposition: {
+        select: {
+          description: true,
+        },
+      },
+      availability: {
+        select: {
+          description: true,
+        },
+      },
+    },
+  });
+
+  profileDogs.forEach((item, index) => {
+    console.log(item);
+    console.log(index);
+  });
+
+  profileAll.forEach((item, index) => {
+    console.log(item);
+    console.log(index);
+  });
+
+  return {
+    props: {
+      profileAll: profileAll,
+    },
+  };
+};
+
+export default function Profiles(getServerSideProps) {
   return (
-    <div>
-      <h1>Profiles</h1>
+    <div className={styles.section}>
+      <div>
+        <h1>Profiles</h1>
+      </div>
+      <div>{getServerSideProps.profileAll.toString()}</div>
     </div>
   );
 }
