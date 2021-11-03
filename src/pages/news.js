3662467000cca2881786922c6baa829cee06f0a0
William Wells <whanwells@gmail.com>
2021-11-02 21:12:30 -0500

    Refactor layout (#26)

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,13 +1,9 @@
-import Layout from "~/components/layout";
-import styles from "~/components/layout.module.css";
+import styles from "~/components/Layout.module.css";
 
 export default function News() {
   return (
-    <>
-      <Layout Home></Layout>
-      <div className={styles.section}>
-        <div>News</div>
-      </div>
-    </>
+    <div className={styles.section}>
+      <div>News</div>
+    </div>
   );
 }
--- src/pages/profiles.js
+++ src/pages/profiles.js
@@ -1,12 +1,7 @@
-import Layout from "~/components/layout";
-
 export default function Profiles() {
   return (
-    <>
-      <Layout Home></Layout>
-      <div>
-        <h1>Profiles</h1>
-      </div>
-    </>
+    <div>
+      <h1>Profiles</h1>
+    </div>
   );
 }
