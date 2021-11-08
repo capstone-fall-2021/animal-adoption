72c1e122c454094f34ce7771a15582450c3e8269
William Wells <whanwells@gmail.com>
2021-11-07 20:30:05 -0600

    Refactor user registration

---
+++ test/lib/user.test.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import prisma from "~/lib/prisma";
+import { userEmailExists, hashPassword, registerUser } from "~/lib/user";
+
+jest.mock("bcrypt");
+
+jest.mock("~/lib/prisma", () => ({
+  user: {
+    count: jest.fn(),
+    create: jest.fn(),
+  },
+}));
+
+describe("userEmailExists", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns true if the email exists", async () => {
+    prisma.user.count.mockResolvedValue(1);
+    const result = await userEmailExists("foo@example.com");
+    expect(result).toBe(true);
+  });
+
+  it("returns false if the email does not exist", async () => {
+    prisma.user.count.mockResolvedValue(0);
+    const result = await userEmailExists("foo@example.com");
+    expect(result).toBe(false);
+  });
+});
+
+describe("hashPassword", () => {
+  it("hashes the password with 10 salt rounds", async () => {
+    await hashPassword("foo");
+    expect(bcrypt.hash).toHaveBeenCalledWith("foo", 10);
+  });
+});
+
+describe("registerUser", () => {
+  it("creates a user with the correct data", async () => {
+    await registerUser({
+      email: "foo@example.com",
+      hash: "bar",
+    });
+
+    expect(prisma.user.create).toHaveBeenCalledWith({
+      data: {
+        email: "foo@example.com",
+        password: "bar",
+      },
+    });
+  });
+});
