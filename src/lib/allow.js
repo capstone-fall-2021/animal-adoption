7bc7b7e23d4a7db5ed26c554bf6069026e194f14
William Wells <whanwells@gmail.com>
2021-11-07 20:50:57 -0600

    Fix "API resolved without sending a response"

--- src/lib/allow.js
+++ src/lib/allow.js
@@ -1,6 +1,6 @@
-const allow = (methods, next) => (req, res) => {
+const allow = (methods, next) => async (req, res) => {
   if (methods.includes(req.method)) {
-    next(req, res);
+    await next(req, res);
   } else {
     res.setHeader("Allow", methods);
     res.status(405).end(`Method ${req.method} Not Allowed`);
