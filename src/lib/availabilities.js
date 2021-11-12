bcb3ec49d9417caf47c26ffcffbe838803324674
stillaschoolgirl <stillaschoolgirl@yahoo.com>
2021-11-11 21:18:26 -0600

    Remove files that don't belong to the review

--- src/lib/availabilities.js
+++
@@ -1,9 +0,0 @@
-import prisma from "~/lib/prisma";
-
-export const getAvailability = () => {
-  return prisma.availability.findMany({
-    select: {
-      description: true,
-    },
-  });
-};
--- src/lib/dispositions.js
+++
@@ -1,9 +0,0 @@
-import prisma from "~/lib/prisma";
-
-export const getAllDispositions = () => {
-  return prisma.disposition.findMany({
-    select: {
-      description: true,
-    },
-  });
-};
