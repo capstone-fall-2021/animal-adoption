18e7a911c459f753800dcca6937dbaff7a417fce
stillaschoolgirl <stillaschoolgirl@yahoo.com>
2021-11-11 22:37:44 -0600

    Add availability query

--- src/pages/availabilities.js
+++ src/pages/availabilities.js
@@ -1,9 +1,9 @@
 import styles from "~/components/Layout.module.css";
 import { useSession } from "next-auth/react";
-import { getAllAvailabilities } from "~/lib/availabilities";
+import { getAvailability } from "~/lib/availabilities";
 
 export const getServerSideProps = async () => {
-  const allAvailabilities = await getAllAvailabilities();
+  const allAvailabilities = await getAvailability();
   return {
     props: {
       allAvailabilities,
@@ -13,12 +13,25 @@ export const getServerSideProps = async () => {
 
 export default function Availabilities({ allAvailabilities }) {
   const { status } = useSession();
+  const availabilitiesAllDisplay = allAvailabilities.map(function (item) {
+    return (
+      <tr key={item.id}>
+        <td>{item.description}</td>
+      </tr>
+    );
+  });
 
   if (status === "authenticated") {
     return (
       <div className={styles.section}>
         <div>
           <h1>Availabilities</h1>
+          <table border="2">
+            <tr>
+              <th>Description</th>
+            </tr>
+            {availabilitiesAllDisplay}
+          </table>
         </div>
       </div>
     );
