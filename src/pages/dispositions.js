be8fc866772317de445ea3d7304f3a8322be643f
William Wells <whanwells@gmail.com>
2021-11-18 20:37:45 -0600

    Use consistent file names

--- src/pages/dispositions.js
+++ src/pages/dispositions.js
@@ -1,4 +1,4 @@
-import styles from "~/components/Layout.module.css";
+import styles from "~/components/layout.module.css";
 import { useSession } from "next-auth/react";
 import { getDispositions } from "~/lib/disposition";
 
