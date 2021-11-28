f03778cfaf04e1dacd0c0e55d21d249301e45c8e
William Wells <whanwells@gmail.com>
2021-11-28 10:10:45 -0600

    Extract `AdminNavbar`

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,19 +1,5 @@
-import styles from "~/components/layout.module.css";
+import { AdminNavbar } from "~/components/admin";
 
 export default function Index() {
-  return (
-    <div className={styles.section}>
-      <div>
-        <h1>Admin</h1>
-        <div className={styles.admin_nav_background}>
-          <a className={styles.admin_nav_links} href="admin/dispositions">
-            Disposition
-          </a>
-          <a className={styles.admin_nav_links} href="admin/types">
-            Type
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+  return <AdminNavbar />;
 }
