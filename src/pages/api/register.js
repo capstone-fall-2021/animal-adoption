c7e69ddfc412574db81064f178e5b0122ea8425e
William Wells <whanwells@gmail.com>
2021-11-27 23:41:25 -0600

    Hoist handling of validation errors

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -5,20 +5,10 @@ import { registration } from "~/schemas";
 const handler = connect();
 
 handler.post(async (req, res) => {
-  try {
-    await registration.validate(req.body, {
-      stripUnknown: true,
-    });
-    const { email, password } = req.body;
-    const user = await createUser(email, password);
-    res.status(201).json(user);
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(400).json({ error: err.errors[0] });
-    } else {
-      throw err;
-    }
-  }
+  await registration.validate(req.body, { stripUnknown: true });
+  const { email, password } = req.body;
+  const user = await createUser(email, password);
+  res.status(201).json(user);
 });
 
 export default handler;
