cb8ce2309ce04ec291ac927ab83acc74bf95d5b2
William Wells <whanwells@gmail.com>
2021-11-28 23:15:33 -0600

    Simplify profile images (#66)

--- src/components/profiles/ProfileImage.js
+++ src/components/profiles/ProfileImage.js
@@ -1,24 +1,8 @@
-import Image from "next/image";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
 
 export default function ProfileImage({ path, alt }) {
-  const [src, setSrc] = useState("");
-
-  useEffect(() => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => setSrc(reader.result), false);
-
-    fetch(path)
-      .then((response) => response.blob())
-      .then((blob) => reader.readAsDataURL(blob));
-  }, [path]);
-
-  if (!src) {
-    return null;
-  }
-
-  return <Image src={src} alt={alt} width="200" height="150" />;
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src={path} alt={alt} width="200" height="150" />;
 }
 
 ProfileImage.propTypes = {
