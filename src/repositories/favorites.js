47f73e391eb5ea08bf05b59662d8108b8a11aab2
William Wells <whanwells@gmail.com>
2021-11-29 17:59:59 -0600

    Add API call to interested button

--- src/repositories/favorites.js
+++ src/repositories/favorites.js
@@ -1,6 +1,17 @@
 import prisma from "~/prisma";
 
-export function createFavorite(userId, profileId) {
+export async function createFavorite(userId, profileId) {
+  const count = await prisma.favorite.count({
+    where: {
+      userId,
+      profileId,
+    },
+  });
+
+  if (count) {
+    return {};
+  }
+
   return prisma.favorite.create({
     data: {
       userId,
