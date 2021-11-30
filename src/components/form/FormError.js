03fa15c4fdae49839134a7b9ae489a8946faccb3
asuezaki <andrewsuezaki@gmail.com>
2021-11-29 17:19:44 -0800

    style about page

--- src/components/form/FormError.js
+++ src/components/form/FormError.js
@@ -1,11 +1,16 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const ErrorMessage = styled.span`
+  color: crimson;
+`;
 
 export default function FormError({ error }) {
   if (typeof error === "undefined" || error === null) {
     return null;
   }
 
-  return <span>{error.message}</span>;
+  return <ErrorMessage>{error.message}</ErrorMessage>;
 }
 
 FormError.propTypes = {
--- src/components/users/NewUserLanding.js
+++ src/components/users/NewUserLanding.js
@@ -1,21 +1,30 @@
 import { RegistrationForm } from "./";
+import styled from "styled-components";
+
+const AboutText = styled.p`
+  margin-left: 15%;
+  margin-right: 15%;
+`;
 
 export default function NewUserLanding() {
   return (
     <div>
-      <h1>About</h1>
-      <p>
-        This website is for people looking for animal companionship. Animal
-        profiles are created in a way that is similar to how they would on a
-        dating site. Different animals are better suited to different people,
-        and even amongst the same species, there are personality differences
-        that can be important when it comes to compatibility. This site hopes to
-        provide a place where you can be informed about the personality of each
-        animal before making a decision to adopt them.
-      </p>
+      <center>
+        <h1>About</h1>
+        <AboutText>
+          Welcome to the Animal Adoption Dating App! {<br />} This website is
+          for people looking for animal companionship. Animal profiles are
+          created in a way that is similar to how they would on a dating site.
+          Different animals are better suited to different people, and even
+          amongst the same species, there are personality differences that can
+          be important when it comes to compatibility. This site hopes to
+          provide a place where you can be informed about the personality of
+          each animal before making a decision to adopt them.
+        </AboutText>
 
-      <h1>Sign up today!</h1>
-      <RegistrationForm />
+        <h1>Sign up today!</h1>
+        <RegistrationForm />
+      </center>
     </div>
   );
 }
