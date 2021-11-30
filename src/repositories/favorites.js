36ddcf524b16caa16c7422d06d22e06c7bbd62c6
William Wells <whanwells@gmail.com>
2021-11-29 18:12:56 -0600

    Add favorites to account page

--- src/repositories/favorites.js
+++ src/repositories/favorites.js
@@ -1,5 +1,22 @@
 import prisma from "~/prisma";
 
+export function getFavoritesByUserId(userId) {
+  return prisma.favorite.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      id: true,
+      profile: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+}
+
 export async function createFavorite(userId, profileId) {
   const count = await prisma.favorite.count({
     where: {
