f6eee3cd1e165b870e3d26b83e3bd5d219399eec
William Wells <whanwells@gmail.com>
2021-11-30 00:45:47 -0600

    Fix admin access (#81)

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,4 +1,7 @@
 import { AdminNavbar } from "~/components/admin";
+import { withAdminSession } from "~/session";
+
+export const getServerSideProps = withAdminSession();
 
 export default function Index() {
   return <AdminNavbar />;
